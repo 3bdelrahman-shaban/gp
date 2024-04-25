2f85be8629a45920245fe11f115a7a669f4c2598
3bdelrahman-shaban <123329497+3bdelrahman-shaban@users.noreply.github.com>
2024-04-25 14:47:34 +0200

    Update main.js

--- nodejs/main.js
+++ nodejs/main.js
@@ -73,8 +73,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
         console.log(`child process exited with code ${code}`);
     });
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(3001, () => {
+    console.log("Server is Running")
+})
 module.exports = app;
